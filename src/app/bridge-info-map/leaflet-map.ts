6e5ef29c00e96d491481781c82d6893abdcfb5f1
Satya Krishna Vinjamuri <satyakrishnavinjamuri868@gmail.com>
2021-03-08 21:24:57 +0530

    My first commit

--- src/app/bridge-info-map/leaflet-map.ts
+++ src/app/bridge-info-map/leaflet-map.ts
@@ -1,5 +1,6 @@
 // See https://leafletjs.com/examples/quick-start/
 import * as Leaflet from 'leaflet';
+import { statesData } from './us-states';
 
 // General location/zoom to see all of Ontario
 const ontario = {
@@ -14,6 +15,7 @@ const defaultZoom = 14;
 export class LeafletMap {
   map: Leaflet.Map;
   popup: Leaflet.Popup;
+  geoJSONLayer: Leaflet.GeoJSON<any>;
 
   constructor(el: string | HTMLElement) {
     this.map = Leaflet.map(el);
@@ -23,6 +25,49 @@ export class LeafletMap {
       .tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
       .addTo(this.map);
 
+    // adding a geoJSON layer so that we can add feature later.
+    var states = JSON.parse(`[{
+    "type": "Feature",
+    "properties": {"party": "Republican"},
+    "geometry": {
+        "type": "Polygon",
+        "coordinates": [[
+            [-104.05, 48.99],
+            [-97.22,  48.98],
+            [-96.58,  45.94],
+            [-104.03, 45.94],
+            [-104.05, 48.99]
+        ]]
+    }
+}, {
+    "type": "Feature",
+    "properties": {"party": "Democrat"},
+    "geometry": {
+        "type": "Polygon",
+        "coordinates": [[
+            [-109.05, 41.00],
+            [-102.06, 40.99],
+            [-102.03, 36.99],
+            [-109.04, 36.99],
+            [-109.05, 41.00]
+        ]]
+    }
+}]`);
+
+Leaflet.geoJSON(statesData, {
+    style: function(feature) {
+        switch (feature.properties.name) {
+            case 'California': return { color: "green" };
+            case 'Idaho':   return { color: "green"};
+            case 'Arizona':   return { color: "green"};
+            case 'Texas':   return { color: "green"};
+            case 'Ohio':   return { color: "green"};
+            case 'Indiana':   return { color: "green"};
+            default: return { color : "none"};
+        }
+    }
+     
+}).addTo(this.map);
     // Show an initial map of Ontario zoomed out
     this.map.setView(ontario.coords, ontario.zoom);
   }
